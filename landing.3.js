3f445f4cc99e9ce7111c524cb8c221743a222a8b
Nischay <nischu1107@gmail.com>
2025-05-24 13:44:33 +0530

    Added the billing codes

---
+++ landing.3.js
@@ -0,0 +1,16 @@
+ <div class="order-actions">
+                    <a href="#thank-you-message" class="btn order-btn">Place Order</a>
+                </div>
+            </form>
+            
+            
+            <div id="thank-you-message" class="thank-you-message">
+                <div class="thank-you-content">
+                    <h3>Thank you for your order,</h3>
+                    <p>Your Token Number is 1. Please wait for your order.</p>
+                    <a href="#menu" class="btn">One more plate??</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
